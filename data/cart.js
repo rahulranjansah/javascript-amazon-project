c8403c7fa0ed8e3734885cf19443989bad2f752a
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2025-06-02 23:01:00 -0400

    loading the cart from local storage

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,25 @@
-export let cart = [
-    {
-        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-    },
-    {
-        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-    }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
 
+if (!cart)
+{
+    cart = [
+        {
+            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+        },
+        {
+            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+        }
+    ];
+}
+
+
+
+function savetoStorage()
+{
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart (productId)
 {
@@ -35,6 +46,8 @@ export function addToCart (productId)
       });
     };
 
+  savetoStorage();
+
 }
 
 export function removeFromCart (productId)
@@ -50,6 +63,8 @@ export function removeFromCart (productId)
     });
 
   cart = newCart;
+
+  savetoStorage();
 
 
 }
