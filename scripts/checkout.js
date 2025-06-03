de881d9b925c1a5acfb86af49a92fee2450b028d
Rahul Ranjan Sah <rahulranjann45@gmail.com>
2025-06-02 22:55:10 -0400

    delete button

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js'
+import {cart, removeFromCart} from '../data/cart.js'
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -22,9 +22,10 @@ cart.forEach((cartItem) =>
 
     console.log(matchingProduct);
 
-
+    if (matchingProduct)
+    {
     cartSummaryHTML +=
-    `<div class="cart-item-container">
+    `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -47,7 +48,7 @@ cart.forEach((cartItem) =>
                 <span class="update-quantity-link link-primary">
                 Update
                 </span>
-                <span class="delete-quantity-link link-primary">
+                <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
                 Delete
                 </span>
             </div>
@@ -100,6 +101,20 @@ cart.forEach((cartItem) =>
         </div>
     </div>
  `;
+    }
 });
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) =>
+{
+ link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    removeFromCart(productId);
+    // console.log(cart);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove();
+ });
+});
